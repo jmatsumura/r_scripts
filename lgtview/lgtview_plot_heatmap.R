cc5f3a3a43a84dc7ca6ae55e61c35e25e567a788
James Matsumura <james.matsumura@gmail.com>
2016-07-22 16:53:23 -0400

    alright, this is where it needs to be for a good while I think. focus again on JS

--- lgtview/lgtview_plot_heatmap.R
+++ lgtview/lgtview_plot_heatmap.R
@@ -9,7 +9,7 @@
 # This script is meant to be invoked from LGTView but it can be run on the
 # command line like so:
 #
-# Rscript lgtview_plot_heatmap.R tax_# metadata_header abundance_type
+# Rscript lgtview_plot_heatmap.R tax_# metadata_header abundance_type noFilter
 #
 # 1. tax_# = taxonomic rank used to build the heatmap. Note that there are
 # often numerous classically unranked assignments made here. Meaning, it
@@ -26,6 +26,9 @@
 # wants relative counts or absolute counts for how many hits to that particular
 # OTU within each of the metadata groups.
 #
+# 4. noFilter = either the path to a file that contains solely a list of read pair
+# IDs or the text 'noFilter' which means all of the metadata file will be used. 
+#
 # Author: James Matsumura
 #
 # Contact: jmatsumura@som.umaryland.edu
@@ -41,28 +44,33 @@ file.path <- args[1]
 taxonomic.rank <- as.numeric(args[2])
 chosen.metadata <- args[3]
 abundance.type <- args[4]
+subset.input <- args[5]
 
 # Start as a data frame until abundances are calculated. Only filling on the
 # off chance that some odd characters were introduced into an OTU name like
 # '#' which causes issues for R's parsing. 
+
 data <- read.table(file=file.path, header=T, sep="\t", fill=T)
 
-# BEGIN SECTION TO BE ADDED ONCE IMPLEMENTED WITH SITE
-#
 # In order to be more accommodating with LGTView, this should take some input that
 # allows for subsetting the particular data file with the current set of data 
 # displayed in LGTView. For now, generate a file that contains a list of a 
 # unique attribute (read pair ID). This makes the script much more flexible as it
 # now benefits from the various filter mechanisms available in LGTView. 
-#
-# subset.input <- args[5]
-# if (subset.input != "noFilter") {
-#   chosen.subset <- read.table(file=subset.input, header=T, fill=T)
-#   filter.dat <- data[chosen.subset,"read"] # use reads to decide which rows to keep
-#   data <- data[filter.dat,] # isolate this subset
-# }
-#
-# END SECTION TO BE ADDED ONCE IMPLEMENTED WITH SITE
+
+if (subset.input != "noFilter") {
+  
+  chosen.subset <- read.table(file=subset.input, header=T)
+  
+  # Odd, can't consistently filter without doing this in some iterative fashion.
+  # Use read IDs to isolate which rows to keep
+  filtered.indices <- c()
+  
+  for(i in 1:length(chosen.subset[[1]])){
+    filtered.indices[i] <- match(chosen.subset[[1]][i], data[,"read"])
+  }
+  data <- data[filtered.indices,]
+}
 
 # X df is whichever metadata is specified
 x.col <- as.data.frame(data[,chosen.metadata])
@@ -77,7 +85,6 @@ f.len <- length(y.col[[1]]) # x.col list is the same length
 # piece of taxonomic information
 final_df <- data.frame(matrix(nrow=length(y.col[,1]),ncol=2))
 final_df[,1] <- x.col # metadata col
-#final_df[,2] <- y.col # this will be done later with the user-entered level of tax
 names(final_df)[1] <- "metadata"
 names(final_df)[2] <- "tax"
 
@@ -107,14 +114,13 @@ for(i in 1:f.len){
   final_df[i,2] <- as.character(final_tax_assignment)
 }
 
-# This next section will build a matrix for the heatmap with each row representing a
+# This next loop will build a matrix for the heatmap with each row representing a
 # unique metadata group and the columns being each unique tax found. 
 #
 # EVENTUALLY UPDATE TO RUN IN PARALLEL IF PROVES TOO SLOW:
 # Going to build individual dfs that each represent a single row for each unique type
 # of metadata present. The amount of columns is then dependent on how many unique 
 # instances of tax were just identified by the last step. 
-#
 
 unique_metadata <- unique(as.character(final_df[,1]))
 unique_tax <- unique(as.character(final_df[,2]))
@@ -127,8 +133,10 @@ idv_final_df <- data.frame(matrix(nrow=length(unique_metadata),ncol=length(uniqu
 rownames(idv_final_df) <- as.character(unique_metadata)
 colnames(idv_final_df) <- as.character(unique_tax) 
 
+max <- 0 # needed to build legend for absolute abundance
+
 # Now, for each piece of metadata, need to calculate abundance for each
-for(i in 1:length(unique_metadata)){ # make this run in parallel
+for(i in 1:length(unique_metadata)){ 
   
   # Subset the data by each unique metadata group
   md_subset <- final_df[,1]==as.character(unique_metadata[i])
@@ -140,19 +148,25 @@ for(i in 1:length(unique_metadata)){ # make this run in parallel
   # Going to build a hash for quick lookup when assigning values to heatmap matrix
   count_hash<-new.env()
   
-  # Already in absolute abundance, if relative desired then convert
+  # Already in absolute abundance, if relative desired then convert. Note that the legend
+  # needs to be a bit tuned for whether the data is for relative or absolute abundances
   if(abundance.type == "relative"){
     
     total <- sum(tax_counts[,2])
     for(q in 1:length(tax_counts[,2])){
       count_hash[[as.character(tax_counts[q,1])]]<-(tax_counts[q,2]/total*100)
     }
-    
+
   # Building the hash slightly different for absolute (no % calc necessary)
   } else {
     
     for(p in 1:length(tax_counts[,2])){
       count_hash[[as.character(tax_counts[p,1])]]<-tax_counts[p,2]
+      
+      if(tax_counts[p,2] > max){
+        max <- tax_counts[p,2]
+      }
+      
     }
   }
   
@@ -171,6 +185,17 @@ for(i in 1:length(unique_metadata)){ # make this run in parallel
       idv_final_df[i,j] <- as.numeric(count_hash[[colnames(idv_final_df)[j]]])
     }
   }
+}
+
+# Build the legend here so it isn't recalculated at each iteration of the previous loop
+if(abundance.type == "relative"){
+  leg <- seq(0, 100, length=21)
+  leg_labels <- c("0%","5%", "10%", "15%", "20%", "25%", "30%",
+                  "35%,", "40%", "45%", "50%", "55%", "60%", "65%",
+                  "70%", "75%", "80%", "85%", "90%", "95%", "100%")
+} else {
+  leg <- seq(0, max, length=21)
+  leg_labels <- round(seq(0, max, length.out=21))
 }
 
 # Now that the data has been populated. 
@@ -197,23 +222,31 @@ if(dim(idv_final_df)[1] < 2){
 # If data can generate a heatmap (greater than 2,2 dimensions), generate it  
 } else {
   
-  png(outfile, width=1440, height=980)
+  png(outfile, width=2400, height=1920)
   
   # Sort by row names to make the graph a bit more legible. Note that, based on 
   # similarity of abundance distribution (which needs to be interpeted carefully)
   # this ordering may be broken down into the different clustered groups.
   sorted_final_df <- idv_final_df[order(rownames(idv_final_df)),]
   
-  # Doing 100 for color gradient to accommodate relative abundance
-  cg <- colorRampPalette(brewer.pal(9,"Blues"))(100)
+  # Change the left margin to accommodate however long the taxonomy covers
+  par(mar=c(5, max(5,max(nchar(colnames(sorted_final_df)))/2.3),5,1))
   
-  # Transform achieves flipping axes
+  # Doing 100 for color gradient to accommodate relative abundance
+  cg <- colorRampPalette(brewer.pal(8,"RdBu"))(100)
+   
+  # Need two images here in order to accommodate layout function
+  layout(matrix(c(2,1), 2, 1), widths=c(0.2,0.2), heights=c(0.4,0.05))
+  
+  image(as.matrix(leg), col=cg, axes=F)
+  axis(1, at=seq(0, 1, length=length(leg)), labels=leg_labels, cex.axis=1)
+
+  # Transform achieves flipping axes.
   # Disabling clustering methods because I do not think it is appropriate to convey
   # relationships regardless of metadata type. Can make this an option in future version.
-  heatmap(as.matrix(t(sorted_final_df)), Rowv=NA, Colv=NA, col=cg, scale="column", margins=c(6,12))
-  
-  # Now build legend to go along side this heatmap
-  leg <- seq(0, 100, length=20)
-  image(as.matrix(leg), col=cg, axes=F)
-  axis(1, at=seq(0,1,length=length(leg)), cex.axis=1)
+  # Not using the heatmap function as using that precludes any use of layout.
+  hm <- as.matrix(t(sorted_final_df))
+  image(1:ncol(hm), 1:nrow(hm), t(hm), col=cg, axes=F, xlab="", ylab="")
+  axis(1, 1:ncol(hm), labels=colnames(hm), las=2)
+  axis(2, 1:nrow(hm), labels=rownames(hm), las=2)
 }
